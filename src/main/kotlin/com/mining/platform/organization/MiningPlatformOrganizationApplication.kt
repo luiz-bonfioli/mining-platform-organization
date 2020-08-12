a7c70b6c4ab5f3fed39e43696815f8b6483f8a4f
Luiz Bonfioli <luiz.bonfioli@gmail.com>
2020-08-12 14:02:23 -0300

    Initial commit

---
+++ src/main/kotlin/com/mining/platform/organization/MiningPlatformOrganizationApplication.kt
@@ -0,0 +1,11 @@
+package com.mining.platform.organization
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MiningPlatformOrganizationApplication
+
+fun main(args: Array<String>) {
+    runApplication<MiningPlatformOrganizationApplication>(*args)
+}
