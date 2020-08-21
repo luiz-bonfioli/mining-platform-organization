ad1ba25030f53f3b95bb3cb529e51dbe51331aec
Luiz Bonfioli <luiz.bonfioli@gmail.com>
2020-08-21 20:35:19 -0300

    Refactoring the architecture

--- src/main/kotlin/com/mining/platform/organization/equipment/category/CategoryRepository.kt
+++ src/main/kotlin/com/mining/platform/organization/equipment/category/CategoryRepository.kt
@@ -1,6 +1,8 @@
 package com.mining.platform.organization.equipment.category
 
 import com.mining.platform.core.datasource.AbstractRepository
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.stereotype.Repository
 import java.util.*
 
@@ -10,4 +12,8 @@ import java.util.*
  * @author luiz.bonfioli
  */
 @Repository
-interface CategoryRepository : AbstractRepository<CategoryEntity, UUID>
+interface CategoryRepository : AbstractRepository<CategoryEntity, UUID> {
+
+    fun findByNameContains(name: String, pageable: Pageable): Page<CategoryEntity>
+
+}
